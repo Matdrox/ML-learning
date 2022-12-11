82c8cd82095e29622507f712a1df92287e561597
Matdrox <Matdrox@users.noreply.github.com>
2022-12-11 12:11:11 +0100

    Setup

---
+++ intro.py
@@ -0,0 +1,11 @@
+'''
+Learning
+Supervised Learning: uses labeled inputs to train models and learn outputs
+Unsupervised Learning: uses patterns from inputs to learn outputs
+Reinforcement Learning: agent learning in an interactive environment based on rewards and penalties
+
+Fatures
+Qualitative: categorical data (finite number of categories or groups, e.g. genders or countries, ages, moods)
+One-hot Encoding: '1' if value matches category
+
+'''
